5be9cbc853de6b0b39b14f474ac969cc7caefb42
joelkur <jkuronen95@gmail.com>
2023-01-17 23:38:15 +0200

    exercise2: refactor board toString as moves down got tricky

--- exercise2/src/Board.mjs
+++ exercise2/src/Board.mjs
@@ -9,14 +9,19 @@ export class Board {
   }
 
   toString() {
-    if (this.block) {
-      return ".X.\n...\n...\n";
-    }
-    return "...\n...\n...\n";
+    let result = "";
+    for (let i = 0; i < this.height * this.width; i++) {
+      if (i > 0 && i % this.width === 0) result += "\n";
+      if (this.block && i === 1) result += "X";
+      else result += ".";
+    };
+    return result + "\n";
   }
 
   /** @param {Block} block */
   drop(block) {
     this.block = block;
   }
+
+  tick() {}
 }
