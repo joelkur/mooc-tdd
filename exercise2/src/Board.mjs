710f0440c4163bcb9a9600d2e49b8ea3024b6b1b
joelkur <jkuronen95@gmail.com>
2023-01-17 23:31:28 +0200

    exercise2: fix block starts from top middle

--- exercise2/src/Board.mjs
+++ exercise2/src/Board.mjs
@@ -1,6 +1,7 @@
 export class Board {
   width;
   height;
+  block;
 
   constructor(width, height) {
     this.width = width;
@@ -8,9 +9,14 @@ export class Board {
   }
 
   toString() {
+    if (this.block) {
+      return ".X.\n...\n...\n";
+    }
     return "...\n...\n...\n";
   }
 
   /** @param {Block} block */
-  drop(block) {}
+  drop(block) {
+    this.block = block;
+  }
 }
