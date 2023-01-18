3e2ea64f068aae7a652b0d87b40c69659f0722d9
joelkur <jkuronen95@gmail.com>
2023-01-18 19:49:05 +0200

    exercise2: fixed i shape orientation

--- exercise2/src/Tetromino.mjs
+++ exercise2/src/Tetromino.mjs
@@ -2,38 +2,65 @@ import { RotatingShape } from './RotatingShape.mjs';
 
 export class Tetromino {
   static get T_SHAPE() {
-    return new Tetromino(
-      `.T.
+    return new Tetromino({
+      initialShape: `.T.
       TTT
-      ...`
-    );
+      ...`,
+      orientationsCount: 4,
+    });
   }
 
   static get I_SHAPE() {
-    return new Tetromino(
-      `.....
+    return new Tetromino({
+      initialShape: `.....
       .....
       IIII.
       .....
-      .....`
-    );
+      .....`,
+      orientationsCount: 2,
+    });
   }
 
-  constructor(shape) {
-    this.shape = typeof shape === "string"
-      ? new RotatingShape(shape)
-      : shape;
+  constructor({initialShape, prerendered = [], orientationsCount = 4, currentOrientation = 0}) {
+    this.orientations = orientationsCount;
+    this.prerendered = prerendered;
+    this.currentOrientation = currentOrientation;
+
+    if (!this.prerendered.length) {
+      let tempShape = new RotatingShape(initialShape);
+      for (let i = 0; i < orientationsCount; i++) {
+        this.prerendered.push(tempShape);
+        tempShape = tempShape.rotateRight();
+      }
+    }
   }
 
   toString() {
-    return this.shape.toString();
+    console.log(this.currentOrientation);
+    return this.prerendered[this.currentOrientation].toString();
   }
 
   rotateRight() {
-    return new Tetromino(this.shape.rotateRight());
+    let newOrientation = this.currentOrientation + 1;
+    if (newOrientation >= this.orientations) {
+      newOrientation = 0;
+    }
+    return new Tetromino({
+      prerendered: this.prerendered,
+      currentOrientation: newOrientation,
+      orientationsCount: this.orientations
+    });
   }
 
   rotateLeft() {
-    return new Tetromino(this.shape.rotateLeft());
+    let newOrientation = this.currentOrientation - 1;
+    if (newOrientation < 0) {
+      newOrientation = this.orientations - 1;
+    }
+    return new Tetromino({
+      prerendered: this.prerendered,
+      currentOrientation: newOrientation,
+      orientationsCount: this.orientations
+    });
   }
 }
