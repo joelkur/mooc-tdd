237c835602faa25dbcbf50beeda09b5dac1cdced
joelkur <jkuronen95@gmail.com>
2023-01-18 19:50:40 +0200

    exercise2: uncomment rest of o shape tests

--- exercise2/test/RotatingTetrominoes.test.mjs
+++ exercise2/test/RotatingTetrominoes.test.mjs
@@ -95,7 +95,7 @@ describe("The O shape", () => {
     );
   });
 
-  xit("cannot be rotated right/clockwise", () => {
+  it("cannot be rotated right/clockwise", () => {
     expect(shape.rotateRight().toString()).to.equalShape(
       `.OO
        .OO
@@ -103,7 +103,7 @@ describe("The O shape", () => {
     );
   });
 
-  xit("cannot be rotated left/counter-clockwise", () => {
+  it("cannot be rotated left/counter-clockwise", () => {
     expect(shape.rotateLeft().toString()).to.equalShape(
       `.OO
        .OO
@@ -111,7 +111,7 @@ describe("The O shape", () => {
     );
   });
 
-  xit("has 1 distinct orientations", () => {
+  it("has 1 distinct orientations", () => {
     expect(distinctOrientations(shape).size).to.equal(1);
   });
 });
