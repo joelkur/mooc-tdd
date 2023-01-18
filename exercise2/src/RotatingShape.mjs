c039b15e2caa673d58b338ec13fd1dc24c8d23c2
joelkur <jkuronen95@gmail.com>
2023-01-18 17:07:04 +0200

    exercise2: replace rotateLeft hard coded value

--- exercise2/src/RotatingShape.mjs
+++ exercise2/src/RotatingShape.mjs
@@ -13,11 +13,14 @@ export class RotatingShape {
     newShape += this.shape.charAt(8) + this.shape.charAt(4) + this.shape.charAt(0) + "\n";
     newShape += this.shape.charAt(9) + this.shape.charAt(5) + this.shape.charAt(1) + "\n";
     newShape += this.shape.charAt(10) + this.shape.charAt(6) + this.shape.charAt(2) + "\n";
-    this.shape = newShape;
-    return this;
+    return newShape;
   }
 
   rotateLeft() {
-    return "CFI\nBEH\nADG\n";
+    let newShape = "";
+    newShape += this.shape.charAt(2) + this.shape.charAt(6) + this.shape.charAt(10) + "\n";
+    newShape += this.shape.charAt(1) + this.shape.charAt(5) + this.shape.charAt(9) + "\n";
+    newShape += this.shape.charAt(0) + this.shape.charAt(4) + this.shape.charAt(8) + "\n";
+    return newShape;
   }
 }
