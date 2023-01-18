d91258269f5baf1626c3de63253620d935d841d5
joelkur <jkuronen95@gmail.com>
2023-01-18 17:02:01 +0200

    exercise2: replace hard coded rotateRight

--- exercise2/src/RotatingShape.mjs
+++ exercise2/src/RotatingShape.mjs
@@ -1,18 +1,20 @@
 export class RotatingShape {
   /** @param {string} shape */
   constructor(shape) {
-    this.shape = shape;
+    this.shape = shape.split("\n").map(row => row.trim()).join("\n") + "\n";
   }
 
   toString() {
-    return this.shape
-      .split("\n")
-      .map(row => row.trim())
-      .join("\n") + "\n";
+    return this.shape;
   }
 
   rotateRight() {
-    return "GDA\nHEB\nIFC\n";
+    let newShape = "";
+    newShape += this.shape.charAt(8) + this.shape.charAt(4) + this.shape.charAt(0) + "\n";
+    newShape += this.shape.charAt(9) + this.shape.charAt(5) + this.shape.charAt(1) + "\n";
+    newShape += this.shape.charAt(10) + this.shape.charAt(6) + this.shape.charAt(2) + "\n";
+    this.shape = newShape;
+    return this;
   }
 
   rotateLeft() {
