0b13ca166a9e2ffb8fd473d73d8d3bc7ab2cecbc
joelkur <jkuronen95@gmail.com>
2023-01-18 17:39:24 +0200

    exercise2: add tetromino t shape

--- exercise2/src/Tetromino.mjs
+++ exercise2/src/Tetromino.mjs
@@ -1,1 +1,11 @@
-export class Tetromino {}
+import { RotatingShape } from './RotatingShape.mjs';
+
+export class Tetromino {
+  static get T_SHAPE() {
+    return new RotatingShape(
+      `.T.
+      TTT
+      ...`
+    );
+  }
+}
